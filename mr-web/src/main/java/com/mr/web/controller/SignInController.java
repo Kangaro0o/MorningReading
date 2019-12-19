758b0a328ab27a1be7214e403fc574add3da5903
LiuWynn <2601659975@qq.com>
2019-12-19 13:30:05 +0800

    签到done!

--- mr-web/src/main/java/com/mr/web/controller/SignInController.java
+++ mr-web/src/main/java/com/mr/web/controller/SignInController.java
@@ -5,12 +5,14 @@ import com.mr.common.ResultStatus;
 import com.mr.mybatis.model.SignIn;
 import com.mr.service.PasswordService;
 import com.mr.service.SignInService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +36,44 @@ public class SignInController {
     @Autowired
     private PasswordService passwordService;
 
+    /**
+     * 用户是否在某天已经签到
+     */
+    @PostMapping(value = "/isSignIn")
+    @ResponseBody
+    public Result<Boolean> isSign(@RequestParam("uid") String uid, @RequestParam("date") String date) {
+        return new Result<>(ResultStatus.SUCCESS, signInService.isSign(uid, date));
+    }
+
+    /**
+     * 签到
+     */
+    @PostMapping(value = "/signIn/uid/{uid}")
+    @ResponseBody
+    public Result<Boolean> signIn(@PathVariable("uid") String uid) {
+        Result<Boolean> result = new Result<>();
+        // 判断是否已经签到
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String today = sdf.format(new Date(System.currentTimeMillis()));
+        // 组装SignIn对象
+        SignIn signIn = new SignIn();
+        signIn.setUid(uid);
+        signIn.setTime(today);
+        // 当天日期存在签到记录时，禁止签到
+        if (signInService.isSign(uid, today)) {
+            result.setCode(ResultStatus.SUCCESS.value());
+            result.setMessage("你已签到！");
+            return result;
+        }
+        if (signInService.signIn(signIn)) {
+            result.setCode(ResultStatus.SUCCESS.value());
+            result.setMessage("签到成功");
+        } else {
+            result.setCode(ResultStatus.ERROR.value());
+            result.setMessage("签到失败，请稍后重试");
+        }
+        return result;
+    }
 
     /**
      * 获取某个用户的所有签到列表
@@ -41,7 +81,7 @@ public class SignInController {
     @ResponseBody
     @GetMapping(value = "/getSignList/id/{id}")
     public Result<List<String>> getList(@PathVariable("id") String id) {
-        List<String> res =  signInService.getList(id);
+        List<String> res = signInService.getList(id);
         return new Result<>(ResultStatus.SUCCESS, res);
     }
 
