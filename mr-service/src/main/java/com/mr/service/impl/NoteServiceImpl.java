bf72551d77531a5812d062e136bd39f5854ed886
lql <1036110216@qq.com>
2019-12-19 14:35:37 +0800

    备忘录数据库方法

---
+++ mr-service/src/main/java/com/mr/service/impl/NoteServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mr.service.impl;
+
+import com.mr.mybatis.mapper.NoteMapper;
+import com.mr.mybatis.model.Note;
+import com.mr.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author lql
+ * @date 2019/12/18 23:32
+ */
+@Service
+public class NoteServiceImpl implements NoteService {
+
+    @Autowired
+    private NoteMapper noteMapper;
+    @Override
+    public boolean saveNote(Note note) {
+
+        return noteMapper.insert(note)!=0;
+    }
+
+    @Override
+    public List<Note> findByUid(String uid) {
+        return  noteMapper.findByUid(uid);
+    }
+
+    @Override
+    public Note findByNid(Integer nid) {
+        return noteMapper.findByNid(nid);
+
+    }
+
+    @Override
+    public boolean deleteByNid(Integer nid) {
+
+        return noteMapper.deleteByNid(nid)!=0;
+    }
+}
